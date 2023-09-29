ade5953d3e8795c8daaa3191b99d4e2d76cc6a86
Cobalt6700 <44681021+Cobalt6700@users.noreply.github.com>
2023-09-29 11:48:40 +0100

    Removed old code after MCU testing

--- src/Encoder_esp32.cpp
+++ src/Encoder_esp32.cpp
@@ -26,7 +26,6 @@ void IRAM_ATTR Encoder_esp32::readEncoder_ISR()
 #endif
 {
 
-	// unsigned long now = millis();
 #if defined(ESP8266)
 #else
 	portENTER_CRITICAL_ISR(&(this->mux));
@@ -41,67 +40,20 @@ void IRAM_ATTR Encoder_esp32::readEncoder_ISR()
 
 		this->old_AB |= (ENC_PORT & 0x03); // add current state
 
-		this->encoder0Pos += ( this->enc_states[( this->old_AB & 0x0f )]);
-		// int8_t currentDirection = (this->enc_states[(this->old_AB & 0x0f)]); //-1,0 or 1
+		this->encoder0Pos += ( this->enc_states[( this->old_AB & 0x0f )]);		
 
-		// if (currentDirection != 0)
-		// {
-		// 	// bool ignoreCorrection = false;
-		// 	// if (this->encoder0Pos > this->_maxEncoderValue) ignoreCorrection = true;
-		// 	// if (this->encoder0Pos < this->_minEncoderValue) ignoreCorrection = true;
-		// 	long prevRotaryPosition = this->encoder0Pos / this->encoderSteps;
-		// 	this->encoder0Pos += currentDirection;
-		// 	long newRotaryPosition = this->encoder0Pos / this->encoderSteps;
+			//https://github.com/igorantolic/ai-esp32-rotary-encoder/issues/40
+			/*
+			when circling there is an issue since encoderSteps is tipically 4
+			that means 4 changes for a single roary movement (step)
+			so if maximum is 4 that means _maxEncoderValue is 4*4=16
+			when we detact 18 we cannot go to zero since next 2 will make it wild
+			Here we changed to 18 set not to 0 but to -2; 17 to -3...
+			Now it seems better however that -3 divided with 4 will give -1 which is not regular -> also readEncoder() is changed to give allowed values
+			It is not yet perfect for cycling options but it is much better than before
 
-		// 	if (newRotaryPosition != prevRotaryPosition && rotaryAccelerationCoef > 1)
-		// 	{
-		// 		// additional movements cause acceleration?
-		// 		//  at X ms, there should be no acceleration.
-		// 		unsigned long accelerationLongCutoffMillis = 200;
-		// 		// at Y ms, we want to have maximum acceleration
-		// 		unsigned long accelerationShortCutffMillis = 4;
-
-		// 		// compute linear acceleration
-		// 		if (currentDirection == lastMovementDirection &&
-		// 			currentDirection != 0 &&
-		// 			lastMovementDirection != 0)
-		// 		{
-		// 			// ... but only of the direction of rotation matched and there
-		// 			// actually was a previous rotation.
-		// 			unsigned long millisAfterLastMotion = now - lastMovementAt;
-
-		// 			if (millisAfterLastMotion < accelerationLongCutoffMillis)
-		// 			{
-		// 				if (millisAfterLastMotion < accelerationShortCutffMillis)
-		// 				{
-		// 					millisAfterLastMotion = accelerationShortCutffMillis; // limit to maximum acceleration
-		// 				}
-		// 				if (currentDirection > 0)
-		// 				{
-		// 					this->encoder0Pos += rotaryAccelerationCoef / millisAfterLastMotion;
-		// 				}
-		// 				else
-		// 				{
-		// 					this->encoder0Pos -= rotaryAccelerationCoef / millisAfterLastMotion;
-		// 				}
-		// 			}
-		// 		}
-		// 		this->lastMovementAt = now;
-		// 		this->lastMovementDirection = currentDirection;
-		// 	}
-
-						//https://github.com/igorantolic/ai-esp32-rotary-encoder/issues/40
-						/*
-						when circling there is an issue since encoderSteps is tipically 4
-						that means 4 changes for a single roary movement (step)
-						so if maximum is 4 that means _maxEncoderValue is 4*4=16
-						when we detact 18 we cannot go to zero since next 2 will make it wild
-						Here we changed to 18 set not to 0 but to -2; 17 to -3...
-						Now it seems better however that -3 divided with 4 will give -1 which is not regular -> also readEncoder() is changed to give allowed values
-						It is not yet perfect for cycling options but it is much better than before
-
-						optimistic view was that most of the time encoder0Pos values will be near to N*encodersteps
-						*/
+			optimistic view was that most of the time encoder0Pos values will be near to N*encodersteps
+			*/
 			// respect limits
 			if ((this->encoder0Pos/ this->encoderSteps) > (this->_maxEncoderValue/ this->encoderSteps)){
 				// Serial.print("circle values limit HIGH");
@@ -138,8 +90,7 @@ void IRAM_ATTR Encoder_esp32::readEncoder_ISR()
 				// Serial.print("no circle values limit ");
 				// Serial.println(this->encoder0Pos);
 			}
- 			//Serial.println(this->encoder0Pos);
-		// }
+ 			//Serial.println(this->encoder0Pos);		
 	}
 #if defined(ESP8266)
 #else
@@ -147,76 +98,13 @@ void IRAM_ATTR Encoder_esp32::readEncoder_ISR()
 #endif
 }
 
-
-// #if defined(ESP8266)
-// ICACHE_RAM_ATTR void Encoder_esp32::readButton_ISR()
-// #else
-// void IRAM_ATTR Encoder_esp32::readButton_ISR()
-// #endif
-// {
-// #if defined(ESP8266)
-// #else
-// 	portENTER_CRITICAL_ISR(&(this->buttonMux));
-// #endif
-
-// 	uint8_t butt_state = !digitalRead(this->encoderButtonPin);
-
-// 	if (!this->isEnabled)
-// 	{
-// 		buttonState = BUT_DISABLED;
-// 	}
-// 	else if (butt_state && !this->previous_butt_state)
-// 	{
-// 		this->previous_butt_state = true;
-// 		Serial.println("Button Pushed");
-// 		buttonState = BUT_PUSHED;
-// 	}
-// 	else if (!butt_state && this->previous_butt_state)
-// 	{
-// 		this->previous_butt_state = false;
-// 		Serial.println("Button Released");
-// 		buttonState = BUT_RELEASED;
-// 	}
-// 	else
-// 	{
-// 		buttonState = (butt_state ? BUT_DOWN : BUT_UP);
-// 		Serial.println(butt_state ? "BUT_DOWN" : "BUT_UP");
-// 	}
-
-// #if defined(ESP8266)
-// #else
-// 	portEXIT_CRITICAL_ISR(&(this->buttonMux));
-// #endif
-// }
-
-// Encoder_esp32::Encoder_esp32(uint8_t encoder_APin, uint8_t encoder_BPin, int encoder_ButtonPin, int encoder_VccPin, uint8_t encoderSteps)
-// {
-// 	this->old_AB = 0;
-
-// 	this->encoderAPin = encoder_APin;
-// 	this->encoderBPin = encoder_BPin;
-// 	this->encoderButtonPin = encoder_ButtonPin;
-// 	this->encoderVccPin = encoder_VccPin;
-// 	this->encoderSteps = encoderSteps;
-
-// #if defined(ESP8266)
-// 	pinMode(this->encoderAPin, INPUT_PULLUP);
-// 	pinMode(this->encoderBPin, INPUT_PULLUP);
-// #else
-// 	pinMode(this->encoderAPin, (areEncoderPinsPulldownforEsp32? INPUT_PULLDOWN:INPUT_PULLUP));
-// 	pinMode(this->encoderBPin, (areEncoderPinsPulldownforEsp32? INPUT_PULLDOWN:INPUT_PULLUP));
-// #endif
-// }
-
-// REWRITTEN Button and vcc to be removed. 
+// REWRITTEN Button and vcc removed. 
 Encoder_esp32::Encoder_esp32(uint8_t encoder_APin, uint8_t encoder_BPin)
 {
 	this->old_AB = 0;
 
 	this->encoderAPin = encoder_APin;
 	this->encoderBPin = encoder_BPin;
-	// this->encoderButtonPin = -1;
-	// this->encoderVccPin = -1;
 	this->encoderSteps = 1;
 
 #if defined(ESP8266)
@@ -240,7 +128,6 @@ void Encoder_esp32::setBoundaries(long minEncoderValue, long maxEncoderValue, bo
 // USED
 long Encoder_esp32::readEncoder()
 {
-	//return (this->encoder0Pos / this->encoderSteps);
 	if(this->encoderSteps == 1){
 		if (this->encoder0Pos > this->_maxEncoderValue)
 			return this->_maxEncoderValue;
@@ -262,28 +149,6 @@ void Encoder_esp32::setEncoderValue(long newValue)
 {
 	reset(newValue);
 }
-
-// Not USED - EB_LIB covers
-// long Encoder_esp32::encoderChanged()
-// {
-// 	long _encoder0Pos = readEncoder();
-// 	long encoder0Diff = _encoder0Pos - this->lastReadEncoder0Pos;
-
-// 	this->lastReadEncoder0Pos = _encoder0Pos;
-
-// 	return encoder0Diff;
-// }
-
-// USED
-// void Encoder_esp32::setup(void (*ISR_callback)(void), void (*ISR_button)(void))
-// {
-// 	attachInterrupt(digitalPinToInterrupt(this->encoderAPin), ISR_callback, CHANGE);
-// 	attachInterrupt(digitalPinToInterrupt(this->encoderBPin), ISR_callback, CHANGE);
-// 	// if (this->encoderButtonPin >= 0)
-// 	// 	attachInterrupt(digitalPinToInterrupt(this->encoderButtonPin), ISR_button, RISING);
-// 	// attachInterrupt(digitalPinToInterrupt(this->encoderButtonPin), ISR_button, CHANGE);
-// }
-
 // USED
 void Encoder_esp32::setup(void (*ISR_callback)(void))
 {
@@ -295,39 +160,7 @@ void Encoder_esp32::setup(void (*ISR_callback)(void))
 void Encoder_esp32::begin()
 {
 	this->lastReadEncoder0Pos = 0;
-	// if (this->encoderVccPin >= 0)
-	// {
-	// 	pinMode(this->encoderVccPin, OUTPUT);
-	// 	digitalWrite(this->encoderVccPin, 1); // Vcc for encoder
-	// }
-
-	// // Initialize rotary encoder reading and decoding
-	// this->previous_butt_state = 0;
-	// if (this->encoderButtonPin >= 0)
-	// {
-		
-	// #if defined(ESP8266)
-	// 	pinMode(this->encoderButtonPin, INPUT_PULLUP);
-	// #else
-	// 	pinMode(this->encoderButtonPin,isButtonPulldown?INPUT_PULLDOWN: INPUT_PULLUP);
-	// #endif
-		
-	// }
 }
-
-// NOT USED - TO BE REMOVED
-// ButtonState Encoder_esp32::currentButtonState()
-// {
-// 	return buttonState;
-// }
-
-// NOT USED - TO BE REMOVED
-// ButtonState Encoder_esp32::readButtonState()
-// {
-// 	// ButtonState _buttonState = buttonState;
-// 	// buttonState =
-// 	return buttonState;
-// }
 
 // USED
 void Encoder_esp32::reset(long newValue_)
@@ -346,9 +179,9 @@ void Encoder_esp32::reset(long newValue_)
 // NEW FUNCTION
 int32_t Encoder_esp32::readAndReset() 
 {
-	static long newValue_ = 0;
-	//newValue_ = newValue_ * this->encoderSteps;
+	static long newValue_ = 0;	
 	int32_t returnPos = this->encoder0Pos;
+	newValue_ = newValue_ * this->encoderSteps;
 	this->encoder0Pos = newValue_ +this->correctionOffset;
 	this->lastReadEncoder0Pos = this->encoder0Pos;
 	if (this->encoder0Pos > this->_maxEncoderValue)
@@ -371,46 +204,3 @@ void Encoder_esp32::disable()
 {
 	this->isEnabled = false;
 }
-
-// NOT USED - TO BE REMOVED
-// bool Encoder_esp32::isEncoderButtonClicked(unsigned long maximumWaitMilliseconds)
-// {
-// 	static bool wasTimeouted = false;
-// 	int button = 1 - digitalRead(encoderButtonPin);
-// 	if (!button)
-// 	{
-// 		if (wasTimeouted)
-// 		{
-// 			wasTimeouted = false;
-// 			return true;
-// 		}
-// 		return false;
-// 	}
-// 	delay(30); // debounce
-// 	button = 1 - digitalRead(encoderButtonPin);
-// 	if (!button)
-// 	{
-// 		return false;
-// 	}
-
-// 	// wait release of button but only maximumWaitMilliseconds
-// 	wasTimeouted = false;
-// 	unsigned long waitUntil = millis() + maximumWaitMilliseconds;
-// 	while (1 - digitalRead(encoderButtonPin))
-// 	{
-// 		if (millis() > waitUntil)
-// 		{
-// 			// button not released until timeout
-// 			wasTimeouted = true;
-// 			return false;
-// 		}
-// 	}
-
-// 	return true;
-// }
-
-// NOT USED - TO BE REMOVED
-// bool Encoder_esp32::isEncoderButtonDown()
-// {
-// 	return digitalRead(encoderButtonPin) ? false : true;
-// }
--- src/Encoder_esp32.hpp
+++ src/Encoder_esp32.hpp
@@ -13,18 +13,7 @@
 // Rotary Encocer
 #define Encoder_esp32_DEFAULT_A_PIN 25
 #define Encoder_esp32_DEFAULT_B_PIN 26
-// #define Encoder_esp32_DEFAULT_BUT_PIN 15
-// #define Encoder_esp32_DEFAULT_VCC_PIN -1
 #define Encoder_esp32_DEFAULT_STEPS 1
-
-// typedef enum
-// {
-// 	BUT_DOWN = 0,
-// 	BUT_PUSHED = 1,
-// 	BUT_UP = 2,
-// 	BUT_RELEASED = 3,
-// 	BUT_DISABLED = 99,
-// } ButtonState;
 
 class Encoder_esp32
 {
@@ -37,17 +26,10 @@ private:
 #endif
 	volatile long encoder0Pos = 0;
 
-	// volatile int8_t lastMovementDirection = 0; // 1 right; -1 left
-	// volatile unsigned long lastMovementAt = 0;
-	// unsigned long rotaryAccelerationCoef = 150;
-
 	bool _circleValues = false;
 	bool isEnabled = true;
-
 	uint8_t encoderAPin = Encoder_esp32_DEFAULT_A_PIN;
 	uint8_t encoderBPin = Encoder_esp32_DEFAULT_B_PIN;
-	// int encoderButtonPin = Encoder_esp32_DEFAULT_BUT_PIN;
-	// int encoderVccPin = Encoder_esp32_DEFAULT_VCC_PIN;
 	long encoderSteps = Encoder_esp32_DEFAULT_STEPS;	
 
 	#pragma GCC diagnostic push
@@ -60,21 +42,11 @@ private:
 
 	int8_t old_AB;
 	long lastReadEncoder0Pos;
-	// bool previous_butt_state;
-
-	// ButtonState buttonState;
 
 	int8_t enc_states[16] = {0, -1, 1, 0, 1, 0, 0, -1, -1, 0, 0, 1, 0, 1, -1, 0};
 	void (*ISR_callback)();
-	// void (*ISR_button)();
 
 public:
-	// Encoder_esp32(
-	// 	uint8_t encoderAPin = Encoder_esp32_DEFAULT_A_PIN,
-	// 	uint8_t encoderBPin = Encoder_esp32_DEFAULT_B_PIN,
-	// 	int encoderButtonPin = Encoder_esp32_DEFAULT_BUT_PIN,
-	// 	int encoderVccPin = Encoder_esp32_DEFAULT_VCC_PIN,
-	// 	uint8_t encoderSteps = Encoder_esp32_DEFAULT_STEPS);
 
 	Encoder_esp32(
 		uint8_t encoderAPin = Encoder_esp32_DEFAULT_A_PIN,
@@ -82,7 +54,6 @@ public:
 		);
 	void setBoundaries(long minValue = -100, long maxValue = 100, bool circleValues = false);
 	int correctionOffset=2;
-	// bool isButtonPulldown = false;
 	bool areEncoderPinsPulldownforEsp32 = true;
 #if defined(ESP8266)
 	ICACHE_RAM_ATTR void readEncoder_ISR();
@@ -94,7 +65,6 @@ public:
 #endif
 
 	void setup(void (*ISR_callback)(void));
-	// void setup(void (*ISR_callback)(void), void (*ISR_button)(void));
 	void begin();
 	void reset(long newValue = 0);
 	int32_t readAndReset();
@@ -102,14 +72,6 @@ public:
 	void disable();
 	long readEncoder();
 	void setEncoderValue(long newValue);
-	// long encoderChanged();
-	// ButtonState currentButtonState();
-	// ButtonState readButtonState();
-	// unsigned long getAcceleration() { return this->rotaryAccelerationCoef; }
-	// void setAcceleration(unsigned long acceleration) { this->rotaryAccelerationCoef = acceleration; }
-	// void disableAcceleration() { setAcceleration(0); }
-
-	// bool isEncoderButtonClicked(unsigned long maximumWaitMilliseconds = 300);
-	// bool isEncoderButtonDown();
+	
 };
 #endif
